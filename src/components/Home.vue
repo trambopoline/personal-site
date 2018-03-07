c7beb37848afea0de1c066e51db4bae7f84f90bd
Devin Dodson <git@devindodson.com>
2018-03-06 18:23:06 -0800

    Moving to Vue CLI project

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hero">
+	  <div class="hero-body">
+		  <div class="container">
+			  <h1 class="title">Here is the title</h1>
+			  <h1 class="subtitle">And a subtitle!</h1>
+		  </div>
+	  </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
