d25a5d3de7780795859048fef3bbbfd31fda24e1
Devin Dodson <Devin Dodson>
2018-03-08 08:28:26 -0800

    Removing initial placeholder styling and adding a footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <section class="footer">
+      <div class="container has-text-centered">
+      Copyright © 2018 - Devin Dodson - All Rights Reserved
+    </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
