0dd7c2a7133077ffd9f3fa0c1852c0a187e44959
Devin Dodson <Devin Dodson>
2018-03-08 08:29:43 -0800

    Above-the-fold hero and some placeholder sections

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="hero">
-	  <div class="hero-body">
-		  <div class="container">
-			  <h1 class="title">Here is the title</h1>
-			  <h1 class="subtitle">And a subtitle!</h1>
-		  </div>
-	  </div>
-  </div>
+	<div class="home">
+		<section class="hero is-fullheight is-medium is-primary is-bold">
+			<div class="hero-body">
+				<div class="container has-text-centered">
+					<h1 class="title is-1">Hello, there!</h1>
+					<h1 class="subtitle is-3">I'm Devin.</h1>
+					<h1 class="subtitle is-4">I'm mostly a front-end developer, but I also like to take a stroll through the rest of the stack on occasion.</h1>
+					<div class="container has-text-centered">
+						<a class="button is-primary is-inverted is-outlined" href="#projects">Projects</a>
+						<a class="button is-primary is-inverted is-outlined" href="#skills">Skills</a>
+						<a class="button is-primary is-inverted is-outlined" href="#other-stuff">Other Stuff</a>
+					</div>
+				</div>
+			</div>
+			<div class="hero-foot">
+
+			</div>
+		</section>
+		<section class="section container" id="projects">Projects Placeholder</section>
+		<section class="section container" id="skills">Skills Placeholder</section>
+		<section class="section container" id="other-stuff">Other Stuff Placeholder</section>
+	</div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
