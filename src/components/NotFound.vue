e1f0dd30a4f77d1b2e45e1a9663b6eac3e00f342
Devin Dodson <Devin Dodson>
2018-03-20 12:44:29 -0700

    Added 404 page, switched to history mode

---
+++ src/components/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+		<section class="hero is-fullheight is-medium is-primary is-bold">
+			<div class="hero-body">
+				<div class="container has-text-centered">
+					<h1 class="title is-1">?</h1>
+					<h3 class="subtitle is-3">Sorry, there's nothing here</h3>
+				</div>
+			</div>
+
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'Home',
+      path: "/",
+      name: "Home",
       component: Home
-    }
+    },
+    { path: "*", component: NotFound }
   ]
-})
+});
