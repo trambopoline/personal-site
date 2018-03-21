d0fc911817581c28d3029d525bb63e9226539f3b
Devin Dodson <Devin Dodson>
2018-03-21 10:10:50 -0700

    primary color change

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,19 @@ export default {
 };
 </script>
 
-<style>
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+<style lang="scss">
+
+@import "~bulma/sass/utilities/initial-variables";
+
+$fuschia: #F012BE;
+$primary: $red;
+
+@import '~bulma/';
+// @import "~bulma/sass/utilities/functions";
+// @import "~bulma/sass/utilities/derived-variables";
+// @import "~bulma/sass/utilities/animations";
+// @import "~bulma/sass/utilities/mixins";
+// @import "~bulma/sass/utilities/controls";
+
+
 </style>
